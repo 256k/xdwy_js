e5b6df8ddf16b8174c8dd7659367b44337141551
256k <fayek.helmi@gmail.com>
2022-07-12 22:12:35 -0400

    added local storage saving

--- index.js
+++ index.js
@@ -1,43 +1,67 @@
-const dataList = [];
+// const dataList = [];
+class List {
+    constructor() {
+        this.list = [];
+    }
+    seed() {
+        for (let i = 0; i < 10; i++) {
+            this.list.push(new Activity(i))
+        }
+    }
+}
+
+const dataList = new List;
 
 class Activity {
     constructor(name) {
-        this.id= "id" + name;
-        this.name= name;
-        this.startDate = new Date("2022-06-25");
+        this.id = "id" + name;
+        this.name = name;
+        this.startDate = new Date();
     }
     reset() {
         this.startDate = newDate();
     }
-    duration() {
+    get duration() {
+        return this.calcDuration();
+    }
+    calcDuration() {
         let now = new Date();
-        return Math.round( (now.getTime() - this.startDate.getTime()) / (60*60*24*1000));
+        let answer = Math.round((now.getTime() - this.startDate.getTime()) / (60 * 60 * 24 * 1000));
+        console.log(answer)
+        return answer
     }
 }
 
-for (let i = 0; i < 10; i++) {
-    dataList.push(new Activity(i))
-}
+
 
 
 const app = document.getElementById("app");
 
 function reload() {
     app.innerHTML = "";
-dataList.forEach(element => {
-    let wrapper = document.createElement('div');
-    let title = document.createElement('p');
-    title.innerText =`${element.duration()} days without ${element.name}`;
-    wrapper.innerHTML = title.innerHTML;
-    app.appendChild(wrapper)
-})
+    let list = [];
+    if (typeof localStorage.getItem("dataList") !== undefined) {
+       list = JSON.parse(localStorage.getItem("dataList"));
+    } else {
+        localStorage.setItem("dataList", JSON.stringify(dataList.list));
+    }
+    dataList.list.forEach(element => {
+        console.log(element)
+        let wrapper = document.createElement('div');
+        let title = document.createElement('p');
+        let now = new Date();
+        title.innerText = `${Math.round((now.getTime() - element.startDate.getTime()) / (60 * 60 * 24 * 1000))} days without ${element.name}`;
+        wrapper.innerHTML = title.innerHTML;
+        app.appendChild(wrapper)
+    })
 }
 reload();
 
 document.getElementById("add").addEventListener('click', () => {
     let nameInput = document.getElementById("input");
-    dataList.push(new Activity(nameInput.value))
+    dataList.list.push(new Activity(nameInput.value))
     nameInput.value = "";
+    localStorage.setItem("dataList", JSON.stringify(dataList.list));
     reload();
 
 })
